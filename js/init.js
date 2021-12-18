aea6d8cda6c0884674d0f03d3d97f10bd36a7351
CarolinaGasca <irenegasca0299@gmail.com>
2021-12-17 18:03:19 -0600

    Act1

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDEERKMSmu8ckZuUaoXPdJ6_cIPclkrwPQ",
+  authDomain: "skinmx-7559c.firebaseapp.com",
+  projectId: "skinmx-7559c",
+  storageBucket: "skinmx-7559c.appspot.com",
+  messagingSenderId: "140456794803",
+  appId: "1:140456794803:web:b69f87f7e006a166126ec6",
+  measurementId: "G-LS65MKWVCJ"
+});
